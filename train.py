89099758fe91de174ada56924df164a5632904b3
jonathan <2946443950@qq.com>
2023-12-08 03:17:02 +0800

    add: modifiedNet bs64 ep300 train and evaluate

--- train.py
+++ train.py
@@ -22,7 +22,7 @@ def train(model_name="originNet", model=Net(), batch_size=32, n_epochs=150, loss
     # Initialize DataLoaderManager
     # batch_size = 32
     valid_size = 0.2
-    number_of_workers = 4
+    number_of_workers = 10
     data_loader_manager = DataLoaderManager(batch_size, valid_size, number_of_workers)
     train_loader = data_loader_manager.get_train_loader()
     valid_loader = data_loader_manager.get_valid_loader()
@@ -63,74 +63,73 @@ def train(model_name="originNet", model=Net(), batch_size=32, n_epochs=150, loss
 
         # keep track of training and validation loss
         train_loss = 0.0
-        valid_loss = 0.0
+        train_corrects = 0
+        train_total = 0
 
         ###################
         # train the model #
         ###################
         model.train()
         for data, target in train_loader:
-            # move tensors to GPU if CUDA is available
             if train_on_gpu:
                 data, target = data.cuda(), target.cuda()
-            # clear the gradients of all optimized variables
             optimizer.zero_grad()
-            # forward pass: compute predicted outputs by passing inputs to the model
             output = model(data)
-            # calculate the batch loss
             loss = criterion(output, target)
-            # backward pass: compute gradient of the loss with respect to model parameters
             loss.backward()
-            # perform a single optimization step (parameter update)
             optimizer.step()
-            # update training loss
             train_loss += loss.item() * data.size(0)
+            _, preds = torch.max(output, 1)
+            train_corrects += torch.sum(preds == target.data)
+            train_total += target.size(0)
+
+        train_loss = train_loss / len(train_loader.dataset)
+        train_accuracy = train_corrects.double() / train_total
 
         ######################
         # validate the model #
         ######################
         model.eval()
 
-        # Calculate Train Loss and Validation Loss
-        train_loss = compute_loss(model, train_loader, criterion, 'cuda' if train_on_gpu else 'cpu')
+        # Compute loss and accuracy for both training and validation sets
         valid_loss = compute_loss(model, valid_loader, criterion, 'cuda' if train_on_gpu else 'cpu')
-
-        # Calculate Train Accuracy and Validation Accuracy
-        train_accuracy = compute_accuracy(model, train_loader, 'cuda' if train_on_gpu else 'cpu')
         valid_accuracy = compute_accuracy(model, valid_loader, 'cuda' if train_on_gpu else 'cpu')
 
-        # Calculate Precision, Recall and F1 Score
-        train_precision, train_recall, train_f1 = compute_precision_recall_fscore(model, train_loader,
-                                                                                  'cuda' if train_on_gpu else 'cpu')
-        valid_precision, valid_recall, valid_f1 = compute_precision_recall_fscore(model, valid_loader,
-                                                                                  'cuda' if train_on_gpu else 'cpu')
-
-        metrics = {
+        # Log basic metrics
+        basic_metrics = {
             'train_loss': train_loss,
             'valid_loss': valid_loss,
             'train_accuracy': train_accuracy,
-            'valid_accuracy': valid_accuracy,
-            'train_precision': train_precision,
-            'valid_precision': valid_precision,
-            'train_recall': train_recall,
-            'valid_recall': valid_recall,
-            'train_f1': train_f1,
-            'valid_f1': valid_f1
+            'valid_accuracy': valid_accuracy
         }
+        logger.log_metrics(epoch, basic_metrics)
 
-        logger.log_metrics(epoch, metrics)
-        recorder.update_metrics(epoch, metrics)
-
-        # save model if validation loss has decreased
+        # Save model if validation loss has decreased
         if valid_loss <= valid_loss_min:
             valid_loss_min = valid_loss
             logger.log_message(
                 'Validation loss decreased ({:.6f} --> {:.6f}).  Saving model ...'.format(valid_loss_min, valid_loss))
+            recorder.save()
 
-        recorder.save()
+    # Compute precision, recall, and F1-score after training is complete
+    model.eval()
+    train_precision, train_recall, train_f1 = compute_precision_recall_fscore(model, train_loader,
+                                                                              'cuda' if train_on_gpu else 'cpu')
+    valid_precision, valid_recall, valid_f1 = compute_precision_recall_fscore(model, valid_loader,
+                                                                              'cuda' if train_on_gpu else 'cpu')
+
+    # Log final additional metrics
+    final_metrics = {
+        'train_precision': train_precision,
+        'valid_precision': valid_precision,
+        'train_recall': train_recall,
+        'valid_recall': valid_recall,
+        'train_f1': train_f1,
+        'valid_f1': valid_f1
+    }
+    logger.log_metrics(n_epochs, final_metrics)
 
     save_model(model, model_name)
-
 
 # if __name__ == "__main__":
 #     train()
