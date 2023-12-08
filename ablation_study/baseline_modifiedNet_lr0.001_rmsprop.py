a164004e9d32b2e8fce8f7e4b3cc8cd2f9fed2cf
jonathan <2946443950@qq.com>
2023-12-08 14:37:35 +0800

    add: modifiedNet bs64 ep300 lr 0.001 rmsprop

---
+++ ablation_study/baseline_modifiedNet_lr0.001_rmsprop.py
@@ -0,0 +1,16 @@
+from pytorch_cifar_classification.models.modified_net import Net
+from train import train
+from evaluate import evaluate
+
+batch_size = 64
+n_epochs = 300
+loss_function_name = 'cross_entropy'
+optimizer_name = 'rmsprop'
+learning_rate = 0.001
+momentum = 0.9
+
+model_name = "modifiedNet_bs" + str(batch_size) + "_ep" + str(n_epochs)
+model = Net()
+
+train(model_name, model, batch_size, n_epochs, loss_function_name, optimizer_name, learning_rate, momentum)
+evaluate(model_name, model, batch_size)
